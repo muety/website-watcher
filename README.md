476c734464658ecab57d1d3b2469e1e6520b0e44
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-10-06 09:05:57 +0200

    Make code Python3 compatible. Update Readme and License.

--- README.md
+++ README.md
@@ -1,26 +1,35 @@
 # website-watcher-script
 
-### Summary
-This script will watch a website, save its contents to a specified text file, compares this file's contents to the website contents at the next visit and send an e-mail if there are differences.
+## Summary
+This script watches a website, saves its contents to a specified text file, compares this file's contents to the website contents at the next visit and sends an e-mail if there are differences.
 
-### Description
+**Please note:** This will only work for **static** websites, which are completely rendered on the server. To parse dynamic, JavaScript-powered websites, like Single Page Apps, you would need a tool like [Selenium WebDriver](https://www.seleniumhq.org/projects/webdriver/). If you're interested, please refer to my blog article about [_"Building a cloud-native web scraper using 8 different AWS services"_](https://muetsch.io/building-a-cloud-native-web-scraper-using-8-different-aws-services.html).
+
+## Description
 I made it for the purpose to repeatedly check a specific webpage where university exam results get published so I get notified almost instantly. Another application could be watching on the postal service's shipment tracking or the like.
-The script is very simple and works in a way that it visits a website, saves the entire HTML code into a local file and compares its contents to the potentially new page contents at the next visit. If there was a difference you will be notified via an e-mail. You can specify a threshold for saying how many single-character changes you want to actually be considered a change (maybe some webpages will display the current time at the right bottom, which you want to ignore - if time is displayed like <code>6:45 pm</code> than a theshold of at least 5 would result in ignoring these changes).
+The script is very simple and works in a way that it visits a website, saves the entire HTML code into a local file and compares its contents to the potentially new page contents at the next visit. If there was a difference you will be notified via an e-mail. You can specify a threshold for saying how many single-character changes you want to actually be considered a change (maybe some webpages will display the current time at the right bottom, which you want to ignore - if time is displayed like `6:45 pm` than a theshold of at least 5 would result in ignoring these changes).
 In order to save memory and CPU time in idle (although only very few) the script itself will only run once when executing it and instantly exit after it has finished one website visit. To make it run repeatedly you will have to set up a cron job that simply execute the script.
 
-### Requirements
+## Requirements
 * Linux based system
-* Python 2.7.x to be installed
-* __EITHER:__ sendmail to be installed
-* __OR:__ access to an SMTP server
+* Python 3.6 to be installed
+* A mail server
+  * Option 1: sendmail to be installed
+  * Option 2: access to an SMTP server
 * Cron jobs
 
-### Usage
+## Usage
 * Clone project: `git clone https://github.com/n1try/website-watcher-script`
 * `sudo pip install -r requirements.txt`
 * Adjust constants in `watcher.py`, e.g. set the address and credential for your SMTP server, the path to local sendmail, the recipient email address etc.
 * `chmod +x watcher.py`
 * Create cronjob for your user account: `crontab -e` and add `@hourly /path/to/script/watcher.py <url> <threshold> <use_smtp>`, e.g. `@hourly ~/dev/watcher.py http://google.com 5 true` for hourly visiting google.com, ignoring changes less than 6 characters and sending notification mail via SMTP
 
-##### NOTE
+### NOTE
 When running the script for the first time, you will get an e-mail that there where changes, since there is a difference between the empty file and the entire webiste HMTL code.
+
+## Contributing
+Feel free to contribute! All contributions that add value to the project are welcome. Please check the issues section for bug reports and feature requests.
+
+## License
+MIT @ [Ferdinand Mütsch](https://muetsch.io)
--- watcher.py
+++ watcher.py
@@ -6,31 +6,31 @@ import sys
 import smtplib
 
 # Config - don't touch
-URL = sys.argv[1]
-TOLERANCE = int(sys.argv[2])  # in different characters
-USE_SMTP = sys.argv[3] == 'true' or sys.argv[3] == 'True' or sys.argv[3] == '1' # use SMTP or not (default is local sendmail)
+URL =       sys.argv[1]
+TOLERANCE = int(sys.argv[2])                                                        # in different characters
+USE_SMTP =  sys.argv[3] == 'true' or sys.argv[3] == 'True' or sys.argv[3] == '1'    # use SMTP or not (default is local sendmail)
 TEMP_FILE = '/tmp/watcher_cache.txt'
 
 # Config - adapt to your needs!
-SENDMAIL_PATH = '/usr/sbin/sendmail' # path to local sendmail binary, SMTP is not going to be used
-SENDER_ADDRESS = 'noreply@example.com' # what should be the sender address of the notification mail
-RECIPIENT_ADDRESS = 'you@example.com' # where to send the notification mail to
-MAIL_SUBJECT = 'Something has changed...' # subject line of the mail you'll receive
-SMTP_HOST = 'localhost' # SMTP server hostname or ip
-SMTP_PORT = 25 # SMTP server's port, usually 25
-SMTP_USERNAME = '' # username to authenticate against SMTP server; leave blank if no auth needed
-SMTP_PASSWORD = '' # password to authenticate against SMTP server; leave blank if no auth needed
-SMTP_STARTTLS = True # whether or not the SMTP server requires encrypted connection
+SENDMAIL_PATH =         '/usr/sbin/sendmail'            # path to local sendmail binary, SMTP is not going to be used
+SENDER_ADDRESS =        'noreply@example.com'           # what should be the sender address of the notification mail
+RECIPIENT_ADDRESS =     'you@example.com'               # where to send the notification mail to
+MAIL_SUBJECT =          'Something has changed...'      # subject line of the mail you'll receive
+SMTP_HOST =             'localhost'                     # SMTP server hostname or ip
+SMTP_PORT =             25                              # SMTP server's port, usually 25
+SMTP_USERNAME =         ''                              # username to authenticate against SMTP server; leave blank if no auth needed
+SMTP_PASSWORD =         ''                              # password to authenticate against SMTP server; leave blank if no auth needed
+SMTP_STARTTLS =         True                            # whether or not the SMTP server requires encrypted connection
 
 # Read length of old web page version
 try:
-    f = open(TEMP_FILE, 'r')
-    len1 = len(f.read())
-    f.close()
+    with open(TEMP_FILE, 'r') as f:
+        len1 = len(f.read())
 except:
     len1 = 0
 
 # Read length of current web page version
+# 301 and 302 redirections are resolved automatically
 r = requests.get(URL)
 if r.status_code is not 200:
     print('Could not fetch %s.' % URL)
@@ -40,24 +40,23 @@ else:
 
 # Write new version to file
 try:
-    f = open(TEMP_FILE, 'w')
-    f.write(r.text.encode('utf8'))
-    f.close()
+    with open(TEMP_FILE, 'w') as f:
+        f.write(r.text)
 except Exception as e:
     print('Could not open file %s: %s' % (TEMP_FILE, e))
 
 def send_mail(sender, recipient, subject, text):
-    msg = "From: %s\n" % sender
-    msg += "To: %s\n" % recipient
-    msg += "Subject: %s\n\n" % subject
+    msg = 'From: %s\n' % sender
+    msg += 'To: %s\n' % recipient
+    msg += 'Subject: %s\n\n' % subject
     msg += text
 
     if not USE_SMTP:
         sendmail_location = SENDMAIL_PATH
-        p = os.popen("%s -t" % sendmail_location, "w")
+        p = os.popen('%s -t' % sendmail_location, 'w')
         status = p.close()
         if status != None:
-            print("Sendmail exit status", status)
+            print('Sendmail exit status', status)
     else:
         try:
             smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
@@ -68,10 +67,10 @@ def send_mail(sender, recipient, subject, text):
             if SMTP_USERNAME is not None and SMTP_USERNAME is not '':
                 smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
             smtp.sendmail(SENDER_ADDRESS, [RECIPIENT_ADDRESS], msg)
-            print("Successfully sent email")
+            print('Successfully sent email')
             smtp.close()
         except smtplib.SMTPException as e:
-            print("Error: unable to send email: ", e)
+            print('Error: unable to send email: ', e)
 
 
 diff = abs(len2 - len1)
