79e32fd0b41a989c9f77359d99a8a016c05fe0ac
Nicholas Gao <nicholas.gao96@gmail.com>
2019-10-06 01:12:10 -0700

    Refactored the code to fix #4

--- watcher.py
+++ watcher.py
@@ -1,79 +1,85 @@
 #!/usr/bin/python
-
+import argparse
 import requests
 import os
-import sys
 import smtplib
+import sys
 
-# Config - don't touch
-URL =       sys.argv[1]
-TOLERANCE = int(sys.argv[2])                                                        # in different characters
-USE_SMTP =  sys.argv[3] == 'true' or sys.argv[3] == 'True' or sys.argv[3] == '1'    # use SMTP or not (default is local sendmail)
-TEMP_FILE = '/tmp/watcher_cache.txt'
 
-# Config - adapt to your needs!
-SENDMAIL_PATH =         '/usr/sbin/sendmail'            # path to local sendmail binary, SMTP is not going to be used
-SENDER_ADDRESS =        'noreply@example.com'           # what should be the sender address of the notification mail
-RECIPIENT_ADDRESS =     'you@example.com'               # where to send the notification mail to
-MAIL_SUBJECT =          'Something has changed...'      # subject line of the mail you'll receive
-SMTP_HOST =             'localhost'                     # SMTP server hostname or ip
-SMTP_PORT =             25                              # SMTP server's port, usually 25
-SMTP_USERNAME =         ''                              # username to authenticate against SMTP server; leave blank if no auth needed
-SMTP_PASSWORD =         ''                              # password to authenticate against SMTP server; leave blank if no auth needed
-SMTP_STARTTLS =         True                            # whether or not the SMTP server requires encrypted connection
-
-# Read length of old web page version
-try:
-    with open(TEMP_FILE, 'r') as f:
-        len1 = len(f.read())
-except:
-    len1 = 0
-
-# Read length of current web page version
-# 301 and 302 redirections are resolved automatically
-r = requests.get(URL)
-if r.status_code is not 200:
-    print('Could not fetch %s.' % URL)
-    len2 = 0
-else:
-    len2 = len(r.text)
-
-# Write new version to file
-try:
-    with open(TEMP_FILE, 'w') as f:
-        f.write(r.text)
-except Exception as e:
-    print('Could not open file %s: %s' % (TEMP_FILE, e))
-
-def send_mail(sender, recipient, subject, text):
-    msg = 'From: %s\n' % sender
-    msg += 'To: %s\n' % recipient
-    msg += 'Subject: %s\n\n' % subject
+def send_mail(text, args):
+    msg = 'From: %s\n' % args.sender_address
+    msg += 'To: %s\n' % args.recipient_address
+    msg += 'Subject: %s\n\n' % args.subject
     msg += text
 
-    if not USE_SMTP:
-        sendmail_location = SENDMAIL_PATH
+    if not args.smtp:
+        sendmail_location = args.sendmail_path
         p = os.popen('%s -t' % sendmail_location, 'w')
         status = p.close()
         if status != None:
             print('Sendmail exit status', status)
     else:
         try:
-            smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+            print("test")
+            smtp = smtplib.SMTP(args.smtp_host, args.smtp_port)
+            print("test4")
             smtp.ehlo()
-            if SMTP_STARTTLS:
+            if not args.disable_tls:
                 smtp.starttls()
             smtp.ehlo()
-            if SMTP_USERNAME is not None and SMTP_USERNAME is not '':
-                smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
-            smtp.sendmail(SENDER_ADDRESS, [RECIPIENT_ADDRESS], msg)
+            print("test2")
+            if args.smtp_username is not None and args.smtp_username is not '':
+                smtp.login(args.smtp_username, args.smtp_password)
+            print("test1")
+            smtp.sendmail(args.sender_address, [args.recipient_address], msg)
             print('Successfully sent email')
             smtp.close()
         except smtplib.SMTPException as e:
             print('Error: unable to send email: ', e)
 
 
-diff = abs(len2 - len1)
-if diff > TOLERANCE:
-    send_mail(SENDER_ADDRESS, RECIPIENT_ADDRESS, MAIL_SUBJECT,
-              'Difference is %s characters.\n%s' % (str(diff), URL))
+def main(args):
+    # Read length of old web page version
+    try:
+        with open(args.tmp_file, 'r') as f:
+            len1 = len(f.read())
+    except:
+        len1 = 0
+
+    # Read length of current web page version
+    # 301 and 302 redirections are resolved automatically
+    r = requests.get(args.url)
+    if r.status_code is not 200:
+        print('Could not fetch %s.' % args.url)
+        len2 = 0
+    else:
+        len2 = len(r.text)
+
+    # Write new version to file
+    try:
+        with open(args.tmp_file, 'w') as f:
+            f.write(r.text)
+    except Exception as e:
+        print('Could not open file %s: %s' % (args.tmp_file, e))
+
+    diff = abs(len2 - len1)
+    if diff > args.tolerance:
+        send_mail('Difference is %s characters.\n%s' % (str(diff), args.url), args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', required=True, type=str, help='URL to watch')
+    parser.add_argument('-t', '--tolerance', default=0, type=int, help='Tolerance for sending a mail.')
+    parser.add_argument('--smtp', action='store_true', help='If set, SMTP is used.')
+    parser.add_argument('--sendmail_path', default='/usr/sbin/sendmail', type=str)
+    parser.add_argument('--tmp_file', default='/tmp/watcher_cache.txt', type=str)
+    parser.add_argument('--sender_address', default='noreply@example.com', type=str)
+    parser.add_argument('--recipient_address', required=True, type=str)
+    parser.add_argument('--subject', default='Something has changed', type=str)
+    parser.add_argument('--smtp_host', default='localhost', type=str)
+    parser.add_argument('--smtp_port', default=25, type=int)
+    parser.add_argument('--smtp_username', default='', type=str)
+    parser.add_argument('--smtp_password', default='', type=str)
+    parser.add_argument('--disable_tls', action='store_false')
+    main(parser.parse_args())
